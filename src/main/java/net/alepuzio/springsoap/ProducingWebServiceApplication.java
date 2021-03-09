aeac0f4c0f4a3c1fd05c2aa59e18c2f4832977eb
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2021-03-09 15:09:37 +0100

    loading file

--- src/main/java/net/alepuzio/springsoap/ProducingWebServiceApplication.java
+++ src/main/java/net/alepuzio/springsoap/ProducingWebServiceApplication.java
@@ -3,10 +3,13 @@ package net.alepuzio.springsoap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 /*
  * https://spring.io/guides/gs/producing-web-service/
  * */
 @SpringBootApplication
+//@PropertySource({ "classpath:src/main/resources/config/env-${build.profile.id}.properties" })
+@PropertySource({ "classpath:config/env-personal-${build.profile.id}.properties" })
 public class ProducingWebServiceApplication {
 
 	public static void main(String[] args) {
