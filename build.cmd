c50a970af7474e7c9d1fe5279c9ce3183d9969f6
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2021-03-09 16:36:55 +0100

    WS port is configurable by cli of maven

--- build.cmd
+++ build.cmd
@@ -1,7 +1,5 @@
 REM mvn spring-boot:run -DenvTarget=personal-dev
 REM mvn spring-boot:run -P=personal-dev -f ../../../../pom.xml -e -X
-
-
 REM del  C:\Users\apuzielli\Documents\personale\mio-github\java\spring-soap\spring_soap\target\spring_soap-1.0-SNAPSHOT.zip
 
-mvn spring-boot:run -P=test -f pom.xml -Dname=prova_name_da_maven -e -X
+mvn spring-boot:run -P=test -f pom.xml "-Dname=prova_name_da_maven" -e -X
--- src/main/resources/script/send-file.cmd
+++ src/main/resources/script/send-file.cmd
@@ -1,4 +1,10 @@
 REM  https://spring.io/guides/gs/producing-web-service/
 
-
+echo "develop"
 curl --header "content-type: text/xml" -d @request.xml http://localhost:8080/ws
+
+echo "test"
+curl --header "content-type: text/xml" -d @request.xml http://localhost:8081/ws
+
+echo "produzione"
+curl --header "content-type: text/xml" -d @request.xml http://localhost:8082/ws
